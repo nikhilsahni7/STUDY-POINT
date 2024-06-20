eac2a8ef78b03893fc87ce0ee8df2e4ecfadd9c5
Nikhil Sahni <nikhil.sahni321@gmail.com>
2024-06-20 10:58:30 +0530

    fix some auth issues

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <AppBar />
-      <header className="bg-gray-bg sticky top-0 z-40 p-4 flex">
+      <header className="bg-gray-bg sticky top-0 z-40 p-4 ">
         <SearchBar />
       </header>
       <main>{children}</main>
